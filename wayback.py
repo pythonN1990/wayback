147bbf4c075e0eecdfe0a5a3796146e87d104abc
DEJI <165672127+pythonN1990@users.noreply.github.com>
2026-01-19 22:28:53 +0300

    Add Wayback class for URL scanning and output saving
    Implement Wayback class for scanning URLs using an API and saving output.

---
+++ wayback.py
@@ -0,0 +1,51 @@
+from colorama import Fore
+import subprocess
+import textwrap
+import json
+import os
+import re
+
+class Wayback:
+    def __init__(self):
+        self.script_path=os.path.abspath(__file__)
+        self.script_dir=os.path.dirname(self.script_path)
+        self.script_path=re.sub(r"\\", "/", self.script_dir)
+
+    def launch_scan(self, api):
+        p = subprocess.Popen(
+            ["curl", api],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+            bufsize=1
+        )
+
+        self.urls = []
+        url_count = 0
+        for line in p.stdout:
+            line = line.strip()
+            if "http" in line.lower():
+                url_count += 1
+                self.urls.append(line)
+                print(textwrap.dedent(f"{Fore.RED}[{url_count}] {Fore.WHITE}{line}"))
+
+    def find_scan_type(self, scan):
+        with open(f"{self.script_path}/datas/wayback_api.json", "r") as file:
+            data = json.load(file)
+            try:
+                scan_type = data[scan]
+                return scan_type
+            except:
+                raise Exception("Tarama türü tespit edilemedi.")
+
+    def save_output(self, filename):
+        cwd = os.getcwd()
+        with open(f"{cwd}/{filename}", "+x") as file:
+            try:
+                for i in self.urls:
+                    file.write(f"{i}\n")
+                print(textwrap.dedent(f"{Fore.GREEN}[+] {Fore.WHITE}Dosya oluşturuldu: {filename}"))
+            except Exception as e:
+                raise Exception(e)
+
+api = Wayback()
